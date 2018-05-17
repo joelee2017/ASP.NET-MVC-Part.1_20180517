ced8e2695c96c43b7e98d5d32b2823d23f4e66ae
joe lee <as955133@gmail.com>
2018-05-18 07:31:46 +0800

    建立Interface 類別

---
+++ ASP.NET MVC 專案分層架構 Part.1_20180517/Models/Interface/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_專案分層架構_Part._1_20180517.Models.Interface
+{
+    public class IProductRepository
+    {
+    }
+}
