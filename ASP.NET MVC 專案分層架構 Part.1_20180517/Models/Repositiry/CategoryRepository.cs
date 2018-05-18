df1dd13066aa2e077a7c0618b3cd6c4175b0e3f2
joe lee <as955133@gmail.com>
2018-05-18 15:54:31 +0800

    CategoryRepository 實作完成。
    將CategoryController中呼叫介面方面 ICategoryRepository 呼叫 CategoryRepository  中實作的方法內容

---
+++ ASP.NET MVC 專案分層架構 Part.1_20180517/Models/Repositiry/CategoryRepository.cs
@@ -0,0 +1,96 @@
+﻿using ASP.NET_MVC_專案分層架構_Part._1_20180517.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_專案分層架構_Part._1_20180517.Models.Repositiry
+{
+    public class CategoryRepository : ICategoryRepository, IDisposable
+    {
+        protected NorthwindEntities db
+        {
+            get;
+            private set;
+        }
+
+        public CategoryRepository()
+        {
+            this.db = new NorthwindEntities();
+        }
+
+        public void Create(Categories instance)
+        {
+           if(instance == null)
+            {
+                throw new ArgumentException("instance");
+            }
+           else
+            {
+                db.Categories.Add(instance);
+                this.SaveChanges();
+            }
+        }
+
+        public void Update(Categories instance)
+        {
+            if(instance == null)
+            {
+                throw new ArgumentException("instance");
+            }
+            else
+            {
+                db.Entry(instance).State = EntityState.Modified;
+                this.SaveChanges();
+            }
+        }
+
+        public void Delete(Categories instance)
+        {
+            if(instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+            else
+            {
+                db.Entry(instance).State = EntityState.Deleted;
+                this.SaveChanges();
+            }
+        }             
+
+        public Categories Get(int categoryID)
+        {
+            return db.Categories.FirstOrDefault(x => x.CategoryID == categoryID);
+        }
+
+        public IQueryable<Categories> GetAll()
+        {
+            return db.Categories.OrderBy(x => x.CategoryID);
+        }
+
+        public void SaveChanges()
+        {
+            this.db.SaveChanges();
+        }
+      
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                if(this.db != null)
+                {
+                    this.db.Dispose();
+                    this.db = nll;
+                }
+            }
+        }
+    }
+}
