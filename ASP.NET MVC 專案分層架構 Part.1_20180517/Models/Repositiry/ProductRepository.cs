fc83ac5e895c711a23d18ba4a3437092c7882868
joe lee <as955133@gmail.com>
2018-05-18 16:17:00 +0800

    ProductRepository實作完成。
    ProductControll 呼叫介面 IProductRepository 方法，在呼叫ProductRepository 方法內容

--- ASP.NET MVC 專案分層架構 Part.1_20180517/Models/Repositiry/ProductRepository.cs
+++ ASP.NET MVC 專案分層架構 Part.1_20180517/Models/Repositiry/ProductRepository.cs
@@ -7,7 +7,7 @@ using System.Web;
 
 namespace ASP.NET_MVC_專案分層架構_Part._1_20180517.Models.Repositiry
 {
-    public class CategoryRepository : ICategoryRepository, IDisposable
+    public class ProductRepository : IProductRepository, IDisposable
     {
         protected NorthwindEntities db
         {
@@ -15,25 +15,25 @@ namespace ASP.NET_MVC_專案分層架構_Part._1_20180517.Models.Repositiry
             private set;
         }
 
-        public CategoryRepository()
+        public ProductRepository()
         {
             this.db = new NorthwindEntities();
         }
 
-        public void Create(Categories instance)
+        public void Create(Products instance)
         {
-           if(instance == null)
+            if(instance == null)
             {
                 throw new ArgumentNullException("instance");
             }
-           else
+            else
             {
-                db.Categories.Add(instance);
+                db.Products.Add(instance);
                 this.SaveChanges();
             }
         }
 
-        public void Update(Categories instance)
+        public void Update(Products instance)
         {
             if(instance == null)
             {
@@ -46,9 +46,9 @@ namespace ASP.NET_MVC_專案分層架構_Part._1_20180517.Models.Repositiry
             }
         }
 
-        public void Delete(Categories instance)
+        public void Delete(Products instance)
         {
-            if(instance == null)
+           if(instance == null)
             {
                 throw new ArgumentNullException("instance");
             }
@@ -57,23 +57,22 @@ namespace ASP.NET_MVC_專案分層架構_Part._1_20180517.Models.Repositiry
                 db.Entry(instance).State = EntityState.Deleted;
                 this.SaveChanges();
             }
-        }             
-
-        public Categories Get(int categoryID)
-        {
-            return db.Categories.FirstOrDefault(x => x.CategoryID == categoryID);
         }
 
-        public IQueryable<Categories> GetAll()
+        public Products Get(int productID)
         {
-            return db.Categories.OrderBy(x => x.CategoryID);
+            return db.Products.FirstOrDefault(x => x.ProductID == productID);
+        }
+
+        public IQueryable<Products> GetAll()
+        {
+            return db.Products.Include(p => p.Categories).OrderByDescending(x => x.ProductID);
         }
 
         public void SaveChanges()
         {
             this.db.SaveChanges();
-        }
-      
+        }             
 
         public void Dispose()
         {
@@ -83,9 +82,9 @@ namespace ASP.NET_MVC_專案分層架構_Part._1_20180517.Models.Repositiry
 
         protected virtual void Dispose(bool disposing)
         {
-            if(disposing)
+            if (disposing)
             {
-                if(this.db != null)
+                if (this.db != null)
                 {
                     this.db.Dispose();
                     this.db = null;
